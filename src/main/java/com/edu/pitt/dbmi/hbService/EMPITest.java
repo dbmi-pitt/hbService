c252edf11cbe1fd1ee052a1fda6769efbb480bda
Richard <richmau90@gmail.com>
2018-10-09 14:24:10 -0400

    Updated to match demo'ed version

--- src/main/java/com/edu/pitt/dbmi/hbService/EMPITest.java
+++ src/main/java/com/edu/pitt/dbmi/hbService/EMPITest.java
@@ -23,6 +23,7 @@ package edu.pitt.dbmi.empi;
 import com.initiate.bean.ArrayOfMemIdentWs;
 import com.initiate.bean.MemIdentWs;
 import com.initiate.bean.Member;
+import com.initiate.bean.MemberGetRequest;
 import com.initiate.bean.MemberSearchRequest;
 
 import edu.pitt.dbmi.empi.scheduler.EmpiService;
@@ -40,98 +41,109 @@ import java.text.SimpleDateFormat;
 
 public class EMPITest
 {
- // Reads a file into an ArrayList
- public static ArrayList<String> readFile(String file) throws IOException
- {
-  ArrayList<String> list = new ArrayList<String>();
-  try
-  {
-   FileReader fr = new FileReader(file);
-   BufferedReader br = new BufferedReader(fr);
-   String currentLine;
-   
-   while((currentLine = br.readLine()) != null)
-   {
-    list.add(currentLine);
-   } 
-   br.close();
-   fr.close();
-  }
-  catch (IOException e) 
-  {
-   e.printStackTrace();
-  }
-  return list;
- }
- 
- // Doesn't check current person_ids for duplication
- public static String mysqlOut(String code, String id) 
- {
-  DateFormat df = new SimpleDateFormat("dd-MMM-yy");
-  Date todayDate = Calendar.getInstance().getTime();
-  String date = df.format(todayDate);
-  String query = String.format(
-    "INSERT INTO person_identity (" +
-    "source_id, identity_id, identity_type_cd, " + 
-    "identity_type_title, load_date) " +
-    "VALUES (" +
-    "hbService, %s, 3000, %s, %s)", id, code, date);
-  return query;
- }
- 
- public static void main(String [] args) throws ParseException
- { 
-  // Set up service to EMPI
-  EmpiService svc = new EmpiService();
-  svc.initialze(); 
-  MemberSearchRequest req;
-  MemberSearchResult res;
-  
-  String row[];
-  String fname, empi, code, id;
-  // Retrieve patient info from txt file
-  try 
-  {
-   ArrayList<String> patientList = readFile("EMPI_Scenarios2.txt");
-   patientList.remove(0); // remove header
-   for(int i = 0; i < patientList.size(); i++) {
-    row = patientList.get(i).split("\\t");
-    fname = row[0].replaceAll("\\s+", "");
-    fname = fname.substring(1, fname.length()-1);
-    empi = row[1];
-    if(row.length > 2)
-     req = EmpiUtil.searchByEmpiIdRequest(row[2]);
-    else
-     req = EmpiUtil.searchByEmpiIdRequest(empi);
-    res = EmpiUtil.searchForMembers(svc, req);
-    if(res == null) {
-     req = EmpiUtil.searchByEmpiIdRequest(empi);
-     res = EmpiUtil.searchForMembers(svc, req);
-     if(res == null)
-      continue;
-    }
-    for(Member memb : res.results)
-    {
-     System.out.println(fname);
-     ArrayOfMemIdentWs pids = memb.getMemIdent();
-     for(MemIdentWs pid : pids.getItem())
-     {
-      code = pid.getAttrCode();
-      id = pid. getIdNumber(); 
-      System.out.println("getMemIdnum: " + pid.getMemIdnum());
-      System.out.println("getIdNumber: " + pid.getIdNumber());
-      System.out.println("getMemRecno: " + pid.getMemRecno());
-      System.out.println("getAttrCode: " + pid.getAttrCode());
-      System.out.println();
-     }
-     System.out.println("----------------------");
-    }
-   }
-  }
-  catch(IOException e)
-  {
-   e.printStackTrace();
-  }
- }
- 
+	// Reads a file into an ArrayList
+	public static ArrayList<String> readFile(String file) throws IOException
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		try
+		{
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String currentLine;
+			
+			while((currentLine = br.readLine()) != null)
+			{
+				list.add(currentLine.replaceAll("\"",""));
+			} 
+			br.close();
+			fr.close();
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	// Doesn't check current person_ids for duplication
+	public static String mysqlOut(String code, String id) 
+	{
+		DateFormat df = new SimpleDateFormat("dd-MMM-yy");
+		Date todayDate = Calendar.getInstance().getTime();
+		String date = df.format(todayDate);
+		String query = String.format(
+				"INSERT INTO person_identity (" +
+				"source_id, identity_id, identity_type_cd, " + 
+				"identity_type_title, load_date) " +
+				"VALUES (" +
+				"hbService, %s, 3000, %s, %s)", id, code, date);
+		return query;
+	}
+	
+	public static void main(String [] args) throws ParseException
+	{	
+		// Set up service to EMPI
+		EmpiService svc = new EmpiService();
+		svc.initialze();	
+		MemberSearchRequest req;
+		MemberSearchResult res;
+		
+		String row[], name[];
+		String epicMRN, aouID, fname, lname, gender, phone, tmp, code, id;
+		Date birthDate;
+		// Retrieve patient info from txt file
+		try 
+		{
+			ArrayList<String> patientList = readFile("prod_test");
+			patientList.remove(0); // remove header
+			for(int i = 0; i < patientList.size(); i++) {
+				row = patientList.get(i).split("\\|");
+				epicMRN = row[0];
+				aouID = row[1];
+				tmp = row[2];
+				name = tmp.split(", ");
+				fname = name[0];
+				lname = name[1];
+				birthDate = new SimpleDateFormat("MM/dd/yyyy").parse(row[3]);
+				gender = row[4];
+				phone = row[5];
+				//req = EmpiUtil.searchByMedipacMRNRequest(epicMRN);
+				//req = EmpiUtil.searchByPhoneRequest(phone);
+			    req = EmpiUtil.searchByNameBirthDtRequest(lname, fname, birthDate);
+				//req = EmpiUtil.getByEpicMRNRequest(epicMRN);
+				//req = EmpiUtil.searchByEmpiIdRequest("3566284");
+				res = EmpiUtil.searchForMembers(svc, req);
+				//res = EmpiUtil.getMembers(svc, req); 
+				if(res == null)
+					continue;
+				for(Member memb : res.results)
+				{
+					System.out.println(name[1] + ", " + name[0]);
+					ArrayOfMemIdentWs pids = memb.getMemIdent();
+					if(pids == null) {
+						System.out.println("ArrayOfMemIdentWS NULL");
+						continue;
+					}
+					for(MemIdentWs pid : pids.getItem())
+					{
+						code = pid.getAttrCode();
+						id = pid. getIdNumber(); 
+						System.out.println("getMemIdnum: " + pid.getMemIdnum());
+						System.out.println("getIdNumber: " + pid.getIdNumber());
+						System.out.println("getMemRecno: " + pid.getMemRecno());
+						System.out.println("getAttrCode: " + pid.getAttrCode());
+						System.out.println("getIdIssuer: " + pid.getIdIssuer());
+						System.out.println("getIdSrcRecno " + pid.getIdSrcRecno());
+						System.out.println();
+					}
+					System.out.println("----------------------");
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 }
